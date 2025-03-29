a9ea2e0855c5debd7ed50aefe1305164ef3abf08
Emma123-ayo <emmated7831@gmail.com>
2025-03-29 14:35:11 +0300

    Add files via upload

---
+++ ayo pract.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//function Declaration, Definition, and calling 
+
+//Function declaration 
+
+int add(int a, int b);
+int main() {
+int num1 = 5, int num2 = 10;
+    
+// Function call
+int result = add ("num1,num2");
+printf("sum: %dn", result);
+return 0;
+// function definition
+int 
+}
